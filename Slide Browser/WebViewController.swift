b9d4f5ee8092126f9a054cf27b499cc35dc325ee
Chris Wong <cwong@konradgroup.com>
2017-03-04 17:59:13 -0500

    Added basic slide menu

---
+++ Slide Browser/WebViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WebViewController.swift
+//  Slide Browser
+//
+//  Created by Chris Wong on 2017-03-04.
+//  Copyright © 2017 Chris Wong. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
