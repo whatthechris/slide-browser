44a11b2e542b2388079955093864ab13ca559ce2
Chris Wong <cwong@konradgroup.com>
2017-03-04 17:23:12 -0500

    Initial Commit

---
+++ Slide Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slide Browser
+//
+//  Created by Chris Wong on 2017-03-04.
+//  Copyright © 2017 Chris Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
